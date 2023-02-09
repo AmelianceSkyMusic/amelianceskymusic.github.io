a3dc04363335f6c16178d8c67ec204e45deed838
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-02-09 10:29:12 +0200

    ^ update asmlib

--- src/asmlib/components/Link/Link.tsx
+++ src/asmlib/components/Link/Link.tsx
@@ -2,23 +2,27 @@ import { forwardRef } from 'react';
 
 import asm from 'asm-ts-scripts';
 
+import s from './Link.module.scss';
+
 type ComponentElementType = HTMLAnchorElement;
 
 interface Link extends ReactHTMLElementAttributes<
 ComponentElementType, React.AnchorHTMLAttributes<ComponentElementType>> {
 	display?: TypographyVariants;
+	noUnderline?: boolean;
 }
 
 export const Link = forwardRef<ComponentElementType, Link>(({
-	children,
 	display,
+	noUnderline,
+	children,
 	target = '_blank',
 	rel = 'noreferrer noopener',
 	className,
 	...rest
 }, ref) => (
 	<a
-		className={asm.join(className, display || 'link')}
+		className={asm.join(s.Link, className, display, 'link', noUnderline && s.noUnderline)}
 		ref={ref}
 		target={target}
 		rel={rel}
