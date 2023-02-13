b99450ca003717df261b42fc83febc9dfdc6d97d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-02-13 19:20:46 +0200

    * update useLang hook

--- src/App/hooks/useLang.ts
+++ src/App/hooks/useLang.ts
@@ -1,30 +1,14 @@
 import { useTranslation } from 'react-i18next';
 
-import type { en } from '~app/en';
-import type { uk } from '~app/uk';
+import type { en } from '~app/translation/en';
+import type { uk } from '~app/translation/uk';
 
 export type LangType =
 	& typeof uk
 	& typeof en;
 
-// function getValueByKey<T, K extends keyof T>(k: K, obj: T) {
-// 	return obj[k];
-// }
-
-// const t = getValueByKey('test', { test: 12, b: 'xxx' } as const);
-
-// export function useLang(key: string) {
-// 	const typeObj = { [key]: key };
-// 	const { t } = useTranslation();
-// 	const langObj = t<string, Partial<LangType>>(key, { returnObjects: true });
-// 	// const keyValue = getValueByKey(key as keyof LangType, { [key]: langObj });
-// 	return { [key]: langObj } as Pick<LangType, typeObj>;
-// }
 export function useLang(key: keyof LangType) {
 	const { t } = useTranslation();
 	const langObj = t<string, Partial<LangType>>(key, { returnObjects: true });
-	return { [key]: langObj } as LangType;
+	return { [key]: langObj } as unknown as LangType;
 }
-
-// const langObj = t<string, Pick<LangType, keyof LangType>>(key, { returnObjects: true });
-// return { [key]: langObj } as Pick<LangType, key: keyof LangType>;
