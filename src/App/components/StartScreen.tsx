b8493615422462dc7c8a374b514b63fecc1ec5af
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-06 14:11:54 +0200

    ^: update StartScreen layout

--- src/App/components/StartScreen.tsx
+++ src/App/components/StartScreen.tsx
@@ -1,18 +1,13 @@
-// import { Logo } from './Logo';
-import { Container } from '~/asmlib/components/blocks/Container';
-import { Main } from '~/asmlib/components/blocks/Main';
 import { Typography } from '~/asmlib/components/Typography';
 
 import s from './StartScreen.module.scss';
 
 export function StartScreen() {
 	return (
-		<Main className={s.StartScreen}>
-			<Container className={s.container} gridContainer>
-				<Typography component="p1" style={{ fontSize: '100px' }}>
-					👋
-				</Typography>
-			</Container>
-		</Main>
+		<div className={s.StartScreen}>
+			<Typography component="p1" style={{ fontSize: '100px' }}>
+				👋
+			</Typography>
+		</div>
 	);
 }
