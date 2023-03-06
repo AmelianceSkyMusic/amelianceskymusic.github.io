c3ca6d5b24b8d29d50ab775f3f1a63cd4f0cd985
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-06 14:11:10 +0200

    ^: add adaptive item

--- src/App/components/Layout/Navigation/Navigation.tsx
+++ src/App/components/Layout/Navigation/Navigation.tsx
@@ -3,11 +3,14 @@ import { Nav } from '~/asmlib/components/blocks/Nav';
 import { Section } from '~/asmlib/components/blocks/Section';
 import { Link } from '~/asmlib/components/Link/Link';
 import { useLang } from '~hooks/useLang';
+import { useScreenQuery } from '~hooks/useScreenQuery';
 
 import s from './Navigation.module.scss';
 
 export function Navigation() {
 	const { navigation: t } = useLang('navigation');
+
+	const { isScreenLG } = useScreenQuery();
 
 	return (
 		<Section className={s.Navigation}>
@@ -16,10 +19,11 @@ export function Navigation() {
 					<Link href="#summary" className={s.link} display="h4" noUnderline>{t.summary}</Link>
 					<Link href="#contacts" className={s.link} display="h4" noUnderline>{t.contacts}</Link>
 					<Link href="#experience" className={s.link} display="h4" noUnderline>{t.experience}</Link>
-					<Link href="#soft-skills" className={s.link} display="h4" noUnderline>{t.softSkills}</Link>
+					<Link href="#education" className={s.link} display="h4" noUnderline>{t.education}</Link>
 					<Link href="#hard-skills" className={s.link} display="h4" noUnderline>{t.hardSkills}</Link>
+					<Link href="#soft-skills" className={s.link} display="h4" noUnderline>{t.softSkills}</Link>
 					<Link href="#projects" className={s.link} display="h4" noUnderline>{t.projects}</Link>
-					<Link href="#hobbies" className={s.link} display="h4" noUnderline>{t.hobbies}</Link>
+					{!isScreenLG && <Link href="#hobbies" className={s.link} display="h4" noUnderline>{t.hobbies}</Link>}
 				</Nav>
 			</Container>
 		</Section>
