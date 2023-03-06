cf521859d0a6b14922e668ad7591d03e78b38169
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-06 18:04:50 +0200

    ^: update Projects layout

--- src/App/pages/Home/sections/Projects/Projects.tsx
+++ src/App/pages/Home/sections/Projects/Projects.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-
 import asm from 'asm-ts-scripts';
 
-import { AccordionItem } from '~/asmlib/components/Accordion/AccordionItem';
 import { Block } from '~/asmlib/components/blocks/Block';
 import { Section } from '~/asmlib/components/blocks/Section';
 import { Img } from '~/asmlib/components/Img';
@@ -19,56 +16,57 @@ import cs from '~pages/Home/commonHome.module.scss';
 export function Projects() {
 	const { projects: t } = useLang('projects');
 
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<Section id="projects" className={cs.section}>
+			<Typography component="h2">{t.title}</Typography>
 
-			<AccordionItem className={s.section} heading={t.title} headingComponent="h2" fullwidth active={isOpen} onToggle={() => setIsOpen((prev) => !prev)}>
-				<div className={cs.section}>
-
-					{t.sections.map((section, i) => (
-
-						<Block className={s.sectionContainer} key={section.title + section.subTitle}>
-							<Link
-								className={s.exampleLink}
-								href={projectsList[i].example.deployLink}
-								blank
-							>
-								<Img
-									className={s.previewImg}
-									src={projectsList[i].example.previewImg}
-									alt={section.title}
-								/>
-							</Link>
-							<Block className={cs.sectionItem}>
-								{(section.title || section.subTitle) && (
-									<Block className={cs.sectionTitle}>
-										{section.title
+			{t.sections.map((section, i) => (
+				<Block className={s.sectionContainer} key={section.title + section.subTitle}>
+					{projectsList[i].example.deployLink ? (
+						<Link className={s.exampleLink} href={projectsList[i].example.deployLink} blank>
+							<Img
+								className={asm.join(s.previewImg)}
+								src={projectsList[i].example.previewImg}
+								alt={section.title}
+							/>
+						</Link>
+					) : (
+						<div className={s.exampleLink}>
+							<Img
+								className={asm.join(s.previewImg, s.noLink)}
+								src={projectsList[i].example.previewImg}
+								alt={section.title}
+							/>
+						</div>
+					)}
+					<Block className={cs.sectionItem}>
+						{(section.title || section.subTitle) && (
+							<Block className={cs.sectionTitle}>
+								{section.title
 									&& (
 										<Block className={s.title}>
 											<Typography component="h3">{section.title}</Typography>
-											<Link href={projectsList[i].codeLink} blank>
-												<SvgIcon size="small" icon="icon--github" clickable />
-											</Link>
+											{projectsList[i].codeLink && (
+												<Link href={projectsList[i].codeLink} blank>
+													<SvgIcon size="small" icon="icon--github" clickable />
+												</Link>
+											)}
 										</Block>
 									)}
-										{section.subTitle && <Typography component="subtitle1">{section.subTitle}</Typography>}
-									</Block>
-								)}
-								{section.descriptions.length > 0 && (
-									<Block className={cs.sectionDescription}>
-										{section.descriptions.length > 0
+								{section.subTitle && <Typography component="subtitle1">{section.subTitle}</Typography>}
+							</Block>
+						)}
+						{section.descriptions.length > 0 && (
+							<Block className={cs.sectionDescription}>
+								{section.descriptions.length > 0
 									&& section.descriptions.map((description) => (
 										<Typography component="p1" key={description}>{description}</Typography>
 									))}
-									</Block>
-								)}
 							</Block>
-						</Block>
-					))}
-				</div>
-			</AccordionItem>
+						)}
+					</Block>
+				</Block>
+			))}
 
 		</Section>
 	);
