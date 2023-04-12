ed80c50c5c48a063e140600fe4fc2e84525c4b98
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-12 14:01:13 +0300

    * remove using i18next-browser-languagedetector in i18n config

--- src/App/translation/i18n.ts
+++ src/App/translation/i18n.ts
@@ -1,13 +1,11 @@
 import { initReactI18next } from 'react-i18next';
 
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import { en } from './en';
 import { uk } from './uk';
 
 i18n
-	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		// debug: true,
