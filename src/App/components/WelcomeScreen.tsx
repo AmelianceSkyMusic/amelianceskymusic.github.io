10ab720677c3f0b921bc1bdff305740b5e18b4bd
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-12 23:39:14 +0300

    + add pdf file auto-prediction depending on the language

--- src/App/components/WelcomeScreen.tsx
+++ src/App/components/WelcomeScreen.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 
 import asm from 'asm-ts-scripts';
+import { useRecoilState } from 'recoil';
 
 import { Backdrop } from '~/ameliance-ui/components/Backdrop';
+import { Block } from '~/ameliance-ui/components/blocks';
 import { Button } from '~/ameliance-ui/components/Button/Button';
 import { ButtonLink } from '~/ameliance-ui/components/Button/ButtonLink';
 import { Portal } from '~/ameliance-ui/components/Portal';
 import { Typography } from '~/ameliance-ui/components/Typography';
+import { languageState } from '~app/state/atoms';
 import { useTranslationKey } from '~app/translation/useTranslationKey';
 
 import s from './WelcomeScreen.module.scss';
 
 export function WelcomeScreen() {
 	const t = useTranslationKey('welcomeScreen');
+
+	const [langRecoil] = useRecoilState(languageState);
 
 	const [isShow, setIsShOw] = useState(true);
 	const [isAnimationEnd, setIsAnimationEnd] = useState(false);
@@ -25,21 +30,23 @@ export function WelcomeScreen() {
 
 	return (
 		<Portal>
-			{/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-			<div className={asm.join(s.WelcomeScreen)} onClick={handleClick}>
+			<Block className={asm.join(s.WelcomeScreen)}>
 				<Backdrop
+					onClick={handleClick}
 					opacity={90}
 					show={isShow}
 					onAnimationEnd={() => { if (!isShow) setIsAnimationEnd(true); }}
 				/>
 				{isShow && (
-					<div className={s.buttons}>
-						<Button type="primary">{t.continue}</Button>
+					<Block className={s.buttons}>
+						<Button type="primary" onClick={handleClick}>{t.continue}</Button>
 						<Typography component="p1">{t.or}</Typography>
-						<ButtonLink href="./pdf/Junior Front-end Developer _ Bobomuratov.pdf" type="secondary">{t.pdf}</ButtonLink>
-					</div>
+						{ langRecoil === 'en'
+							? <ButtonLink href="./pdf/Junior Front-end Developer _ Bobomuratov (en).pdf" type="secondary">{t.pdf}</ButtonLink>
+							: <ButtonLink href="./pdf/Junior Front-end Developer _ Bobomuratov (uk).pdf" type="secondary">{t.pdf}</ButtonLink>}
+					</Block>
 				)}
-			</div>
+			</Block>
 		</Portal>
 	);
 }
