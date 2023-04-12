029046ce62ea6086402fdc43fab021761c392751
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-12 13:42:34 +0300

    ^ update photo style

--- src/App/pages/Home/sections/Summary/Summary.tsx
+++ src/App/pages/Home/sections/Summary/Summary.tsx
@@ -1,17 +1,23 @@
+import asm from 'asm-ts-scripts';
+import { useRecoilState } from 'recoil';
+
 import { Block } from '~/ameliance-ui/components/blocks/Block';
 import { Section } from '~/ameliance-ui/components/blocks/Section';
 import { List } from '~/ameliance-ui/components/List/List';
 import { ListItem } from '~/ameliance-ui/components/List/ListItem';
 import { Typography } from '~/ameliance-ui/components/Typography';
+import { themeState } from '~app/state/atoms';
 import { useTranslationKey } from '~app/translation/useTranslationKey';
 
-import photo from '~assets/img/photo.png';
+import photoLight from '~assets/img/photo-light.png';
 
 import s from './Summary.module.scss';
 import cs from '~pages/Home/commonHome.module.scss';
 
 export function Summary() {
 	const t = useTranslationKey('summary');
+	const [themeRecoil] = useRecoilState(themeState);
+	const imgClass = themeRecoil === 'dark' ? s.dark : s.light;
 
 	return (
 		<Section id="heading" className={cs.section}>
@@ -20,7 +26,7 @@ export function Summary() {
 					<Typography component="h1" className={s.title}>{t.name}</Typography>
 					<Typography component="h2" className={s.subtitle}>{t.position}</Typography>
 				</Block>
-				<img className={s.photo} src={photo} alt="avatar" />
+				<img className={asm.join(s.photo, imgClass)} src={photoLight} alt="avatar" />
 			</Block>
 			<Block id="summary" className={cs.sectionDescription}>
 				<Typography component="p1">{t.description.part1}</Typography>
