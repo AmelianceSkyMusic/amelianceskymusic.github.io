84a28c3627a7624c44776925cf590607bd38354a
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-14 17:32:48 +0300

    + add switch dark theme in WelcomeScreen

--- src/App/components/WelcomeScreen.tsx
+++ src/App/components/WelcomeScreen.tsx
@@ -8,10 +8,14 @@ import { Backdrop } from '~/ameliance-ui/components/Backdrop';
 import { Block } from '~/ameliance-ui/components/blocks';
 import { Button } from '~/ameliance-ui/components/Button/Button';
 import { ButtonLink } from '~/ameliance-ui/components/Button/ButtonLink';
+import { Icon } from '~/ameliance-ui/components/Icon';
 import { CheckIcon } from '~/ameliance-ui/components/icons/CheckIcon';
+import { MoonIcon } from '~/ameliance-ui/components/icons/MoonIcon';
+import { SunIcon } from '~/ameliance-ui/components/icons/SunIcon';
 import { Portal } from '~/ameliance-ui/components/Portal';
 import { Typography } from '~/ameliance-ui/components/Typography';
-import { languageState } from '~app/state/atoms';
+import { toggleTheme } from '~/ameliance-ui/scripts/toggleTheme';
+import { languageState, themeState } from '~app/state/atoms';
 import { useTranslationKey } from '~app/translation/useTranslationKey';
 
 import s from './WelcomeScreen.module.scss';
@@ -22,6 +26,13 @@ export function WelcomeScreen() {
 	const t = useTranslationKey('welcomeScreen');
 
 	const [langRecoil] = useRecoilState(languageState);
+
+	const [themeRecoil, setThemeRecoil] = useRecoilState(themeState);
+
+	const handleChangeTheme = () => {
+		const newTheme = toggleTheme();
+		setThemeRecoil(newTheme);
+	};
 
 	const [isShow, setIsShOw] = useState(true);
 	const [isAnimationEnd, setIsAnimationEnd] = useState(false);
@@ -53,21 +64,29 @@ export function WelcomeScreen() {
 				/>
 				{isShow && (
 					<Block className={s.buttons}>
-						<Block className={s.languages}>
-							<Typography
-								className={asm.join(s.language, enActiveClass)}
-								onClick={handleEnLanguageOnClick}
-							>
-								{langRecoil === 'en' && <CheckIcon />}
-								English
-							</Typography>
-							<Typography
-								className={asm.join(s.language, ukActiveClass)}
-								onClick={handleUkLanguageOnClick}
-							>
-								{langRecoil === 'uk' && <CheckIcon />}
-								Українська
-							</Typography>
+						<Block className={s.topPanel}>
+							<Typography component="h1" display="h2" className={s.heading}>CV</Typography>
+							<Block className={s.controls}>
+								<Block className={s.languages}>
+									<Typography
+										className={asm.join(s.language, enActiveClass)}
+										onClick={handleEnLanguageOnClick}
+									>
+										{langRecoil === 'en' && <CheckIcon />}
+										English
+									</Typography>
+									<Typography
+										className={asm.join(s.language, ukActiveClass)}
+										onClick={handleUkLanguageOnClick}
+									>
+										{langRecoil === 'uk' && <CheckIcon />}
+										Українська
+									</Typography>
+								</Block>
+								<Icon className={s.themeIcon} onClick={handleChangeTheme}>
+									{themeRecoil === 'dark' ? <MoonIcon /> : <SunIcon />}
+								</Icon>
+							</Block>
 						</Block>
 						<Button type="primary" onClick={handleClick}>{t.continue}</Button>
 						<Typography component="p1">{t.or}</Typography>
