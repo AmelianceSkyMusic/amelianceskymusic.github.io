d30f3b37c9982b7e8f23cecda6ad86fd5b942a34
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-14 16:24:31 +0300

    * update Project translation implementation

--- src/App/translation/en/projects.ts
+++ src/App/translation/en/projects.ts
@@ -1,7 +1,18 @@
 export const projects = {
 	title: 'PROJECTS',
-	sections: [
-		{
+	sections: {
+		amelianceUI: {
+			title: 'Ameliance UI',
+			subTitle: 'React, React Hook Form, react-responsive',
+			descriptions: [
+				'Own UI library of React components',
+				'The library that has gone from a simple SCSS UI library to a React component library and contains more than 300 files',
+				'The library contains not only components, but also a grid, theme change, and hooks for working with the theme. Supports work with React Hook Form',
+				'Under development and adaptation',
+			],
+			deployComment: '',
+		},
+		wisay: {
 			title: 'Wisay',
 			subTitle: 'React, TypeScript, Zustand, SCSS Modules, Vite, ESLint, Autoprefixer, Ameliance UI, ASM TS Scripts, HLS, React Responsive, React Router DOM',
 			descriptions: [
@@ -10,15 +21,16 @@ export const projects = {
 			],
 			deployComment: '* to view the deploy you need to use the Access-Control-Allow-Origin extension',
 		},
-		{
+		cv: {
 			title: 'CV',
 			subTitle: 'React, TypeScript, Recoil, SCSS Modules, Vite, ESLint, Autoprefixer, Ameliance UI, ASM TS Scripts, I18next, React Responsive, React Router DOM',
 			descriptions: [
 				'My CV',
 				'The project was made on React to practice working with React once again, try something new and hone my skills',
 			],
+			deployComment: '',
 		},
-		{
+		petAW: {
 			title: 'AW',
 			subTitle: 'React, TypeScript, Redux, SCSS Modules, Vite, ESLint, Autoprefixer, Firebase, React Hook Form, Ameliance, ASM TS Scripts, React Responsive, React Router DOM, React Showfall, Google Apps Script',
 			descriptions: [
@@ -26,8 +38,9 @@ export const projects = {
 				'Single Page Application for use within a music group. The application contains a list of songs, chords, and a chat for communication. The app\'s functionality includes the ability to create a song list and transpose chords',
 				'The project was started at the beginning of my studies in vanilla JavaScript and later rewritten in React. Some of the functionality is in the process of being ported to React',
 			],
+			deployComment: '',
 		},
-		{
+		stageReactPMA: {
 			title: 'Project Manager App',
 			subTitle: 'React, TypeScript, Redux, Axios, Lodash, MUI, Create React App, I18next, ESLint, React Hook Form, React Responsive, React Router Dom, React DND',
 			descriptions: [
@@ -35,8 +48,9 @@ export const projects = {
 				'The final task of the React course was performed by a team of 3 people. Due to the lack of electricity and differences in the electricity supply schedule, communication was poor, and the time was less than for teams from outside Ukraine',
 				'My contribution: Team lead, organization of work and communication in the team. UX/UI design of the application. Project setup, installation of packages. Creating an IDE setup and project management guide for the team describing everything in the project, what libraries are used, and how we use them. Development of the application state. Setting up MUI, MUI components, grid, adding light and dark themes. Layout: home page, login, registration. Validation of forms',
 			],
+			deployComment: '',
 		},
-		{
+		stageReactRSS: {
 			title: 'RS School React',
 			subTitle: 'React, TypeScript, Redux, Axios, Create React App, ESLint, Prettier, Rest API, React Hook Form, React Router DOM',
 			descriptions: [
@@ -50,8 +64,9 @@ export const projects = {
 				+ 'Redux RTK, '
 				+ 'React Testing Library',
 			],
+			deployComment: '',
 		},
-		{
+		stageTwoEngle: {
 			title: 'Engle',
 			subTitle: 'React, TypeScript, Redux Vanilla, Axios, SCSS, Create React App, ESLint, React Responsive, React Router DOM, asmlib',
 			descriptions: [
@@ -59,22 +74,25 @@ export const projects = {
 				'The final task of the JavaScript/Front end course was performed by a team of 3 people. Although the course was on JavaScript, with the permission of the mentor, we tried to work with React and Redux for the first time. It was very difficult, but we did it',
 				'My contribution: Team leader, organization of work and communication in the team. UX/UI design of the application. Setting up the project, installing packages. Development of the app store. Creating ui components. Light and dark themes. Implementation of the "Audio Challenge" game. Layout: header, footer, audio challenge game, home scree',
 			],
+			deployComment: '',
 		},
-		{
+		stageTwoAsyncRace: {
 			title: 'Async Race',
 			subTitle: 'TypeScript, HTML, SCSS, Rest API, ESLint, Prettier, Lottie, Web Animation API',
 			descriptions: [
 				'The first TypeScript project where we learned to work with the Rest API in depth. Here I tried a decomposition approach. I also tried working with Lottie animation and the Web Animations API',
 			],
+			deployComment: '',
 		},
-		{
+		stageTwoNewsPortal: {
 			title: 'News Portal',
 			subTitle: 'TypeScript, HTML, SCSS, Rest API',
 			descriptions: [
 				'A website with news. The task was to rewrite the existing code from JavaScript to TypeScript and make a layout of the design',
 			],
+			deployComment: '',
 		},
-		{
+		stageTwoOnlineStore: {
 			title: 'Online Store',
 			subTitle: 'JavaScript, HTML, SCSS, Rest API',
 			descriptions: [
@@ -82,61 +100,69 @@ export const projects = {
 			],
 			deployComment: '* localStorage conflict is possible. In this case, clear localStorage',
 		},
-		{
+		stageOneCssMemSlider: {
 			title: 'Css Mem Slider',
 			subTitle: 'CSS',
 			descriptions: [
 				'Advanced work with CSS',
 			],
+			deployComment: '',
 		},
-		{
+		stageOneVirtualKeyboard: {
 			title: 'Virtual Keyboard',
 			subTitle: 'JavaScript, HTML, SCSS',
 			descriptions: [
 				'Work with keyboard events. Working with audio, creating different themes',
 			],
+			deployComment: '',
 		},
-		{
+		stageOneShelter: {
 			title: 'Shelter',
 			subTitle: 'JavaScript, HTML, SCSS',
 			descriptions: [
 				'Layout of a multi-page website. Creating an endless carousel and pagination',
 			],
+			deployComment: '',
 		},
-		{
+		stageZeroMovieApp: {
 			title: 'Movie App',
 			subTitle: 'JavaScript, HTML, SCSS, Rest API',
 			descriptions: [
 				'Working with the Rest API. Advanced work with UI elements',
 			],
+			deployComment: '',
 		},
-		{
+		stageZero2048: {
 			title: 'Game 2048',
 			subTitle: 'JavaScript, HTML, SCSS',
 			descriptions: [
 				'Working with algorithms. Change the topic. Creating a game state',
 			],
+			deployComment: '',
 		},
-		{
+		stageZeroVideoPlayer: {
 			title: 'Custom Video Player',
 			subTitle: 'JavaScript, HTML, SCSS',
 			descriptions: [
 				'Customization of standard controls. Working with audio',
 			],
+			deployComment: '',
 		},
-		{
+		stageZeroPortfolio: {
 			title: 'Photographer\'s Portfolio',
 			subTitle: 'JavaScript, HTML, CSS',
 			descriptions: [
 				'Layout of a one-page website. Work with data. Change the language. Change the theme',
 			],
+			deployComment: '',
 		},
-		{
+		stageZeroCv: {
 			title: 'RSS CV',
 			subTitle: 'HTML, CSS',
 			descriptions: [
 				'Layout using HTML CSS',
 			],
+			deployComment: '',
 		},
-	],
+	},
 };
