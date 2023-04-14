6b7e83653f56ebba24a4dbdb479e6f1844f107a4
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-14 16:45:34 +0300

    + add switch languages in Welcome Screen

--- src/App/components/WelcomeScreen.tsx
+++ src/App/components/WelcomeScreen.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import asm from 'asm-ts-scripts';
 import { useRecoilState } from 'recoil';
@@ -7,6 +8,7 @@ import { Backdrop } from '~/ameliance-ui/components/Backdrop';
 import { Block } from '~/ameliance-ui/components/blocks';
 import { Button } from '~/ameliance-ui/components/Button/Button';
 import { ButtonLink } from '~/ameliance-ui/components/Button/ButtonLink';
+import { CheckIcon } from '~/ameliance-ui/components/icons/CheckIcon';
 import { Portal } from '~/ameliance-ui/components/Portal';
 import { Typography } from '~/ameliance-ui/components/Typography';
 import { languageState } from '~app/state/atoms';
@@ -15,6 +17,8 @@ import { useTranslationKey } from '~app/translation/useTranslationKey';
 import s from './WelcomeScreen.module.scss';
 
 export function WelcomeScreen() {
+	const navigate = useNavigate();
+
 	const t = useTranslationKey('welcomeScreen');
 
 	const [langRecoil] = useRecoilState(languageState);
@@ -28,6 +32,16 @@ export function WelcomeScreen() {
 
 	if (!isShow && isAnimationEnd) return null;
 
+	const handleEnLanguageOnClick = () => {
+		navigate('/en');
+	};
+
+	const handleUkLanguageOnClick = () => {
+		navigate('/uk');
+	};
+
+	const enActiveClass = langRecoil === 'en' && s.active;
+	const ukActiveClass = langRecoil === 'uk' && s.active;
 	return (
 		<Portal>
 			<Block className={asm.join(s.WelcomeScreen)}>
@@ -39,6 +53,22 @@ export function WelcomeScreen() {
 				/>
 				{isShow && (
 					<Block className={s.buttons}>
+						<Block className={s.languages}>
+							<Typography
+								className={asm.join(s.language, enActiveClass)}
+								onClick={handleEnLanguageOnClick}
+							>
+								{langRecoil === 'en' && <CheckIcon />}
+								English
+							</Typography>
+							<Typography
+								className={asm.join(s.language, ukActiveClass)}
+								onClick={handleUkLanguageOnClick}
+							>
+								{langRecoil === 'uk' && <CheckIcon />}
+								Українська
+							</Typography>
+						</Block>
 						<Button type="primary" onClick={handleClick}>{t.continue}</Button>
 						<Typography component="p1">{t.or}</Typography>
 						{ langRecoil === 'en'
